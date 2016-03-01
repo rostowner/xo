7158c6dc14640e9b4dd03dcd4573f9d019bfe02d
f-xyz <andrey.lischina@gmail.com>
2016-03-01 15:17:57 +0200

    cdn-2

--- js/app.js
+++ js/app.js
@@ -1,22 +1,33 @@
 'use strict';
 
-<<<<<<< HEAD
 var First = React.createClass({
 	componentDidMount: function() {},
 	render: function () {
-		return (<h1>Trololo</h1>);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<td>X</td>
+						<td>X</td>
+						<td>X</td>
+					</tr>
+					<tr>
+						<td>X</td>
+						<td>X</td>
+						<td>X</td>
+					</tr>
+					<tr>
+						<td>X</td>
+						<td>X</td>
+						<td>X</td>
+					</tr>
+				</tbody>
+			</table>
+		);
 	}
 });
 
 ReactDOM.render(
   <First />,
   document.getElementById('xo')
-=======
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-ReactDOM.render(
-  "<h1>Hello, world!</h1>",
-  document.getElementById('xo-example')
->>>>>>> 467905f7e8295ead8b14f55e33959af42a9b024d
 );
