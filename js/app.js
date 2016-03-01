9dab0e4385a4ced543f5d251e3360fdd0ada50fc
f-xyz <andrey.lischina@gmail.com>
2016-03-01 15:11:13 +0200

    cdn

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var First = React.createClass({
+	componentDidMount: function() {},
+	render: function () {
+		return (<h1>Trololo</h1>);
+	}
+});
+
+ReactDOM.render(
+  <First />,
+  document.getElementById('xo')
+);
