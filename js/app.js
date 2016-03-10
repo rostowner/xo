679af26626d43c8fb9dd02be409cdceea9aa0995
Rostyslav <rostowner@gmail.com>
2016-03-10 16:52:51 +0200

    use inner components

--- js/app.js
+++ js/app.js
@@ -1,33 +1,48 @@
 'use strict';
 
-var First = React.createClass({
-	componentDidMount: function() {},
+const Box = React.createClass({
+	getInitialState: function () {
+		return {
+			size: 3,
+			matrix: [
+				['x', 'x', 'x'], 
+				['x', 'x', 'x'], 
+				['x', 'x', 'x']
+			]
+		};
+	},
 	render: function () {
-		return (
-			<table>
-				<tbody>
-					<tr>
-						<td>X</td>
-						<td>X</td>
-						<td>X</td>
-					</tr>
-					<tr>
-						<td>X</td>
-						<td>X</td>
-						<td>X</td>
-					</tr>
-					<tr>
-						<td>X</td>
-						<td>X</td>
-						<td>X</td>
-					</tr>
-				</tbody>
-			</table>
-		);
+		const matrix = this.state.matrix;
+		return (<table>
+					<tbody>
+						{matrix.map(function(row){ 
+							return <Row cells={row} />; 
+						})}
+					</tbody>
+				</table>
+			);
+	}
+});
+
+const Row = React.createClass({
+	render: function () {
+		const cells = this.props.cells;
+		return (<tr>
+				{cells.map(function(cell){
+					return <Cell item={cell} />;
+				})}
+			</tr>);
+	}
+});
+
+const Cell = React.createClass({
+	render: function () {
+		const item = this.props.item;
+		return (<td>{item}</td>);
 	}
 });
 
 ReactDOM.render(
-  <First />,
+  <Box />,
   document.getElementById('xo')
 );
