1e99fde2c568fcf626ee1cd2a14cd279b085988f
Rostislav <rostowner@gmail.com>
2016-03-17 07:41:33 +0200

    cool, now you can set X

--- js/app.js
+++ js/app.js
@@ -17,13 +17,18 @@ const Box = React.createClass({
 			]
 		};
 	},
+	changeStateOfCell: function (x, y) {
+		var matrix = this.state.matrix;
+		matrix[x][y] = cellStates.x;
+		this.setState({matrix: matrix});
+	},
 	render: function () {
 		const matrix = this.state.matrix;
 		return (<table>
 					<tbody>
 						{matrix.map(function(row, index){ 
-							return <Row cells={row} rowindex={index} />; 
-						})}
+							return <Row cells={row} changeStateOfCell={this.changeStateOfCell} rowindex={index} />; 
+						}, this)}
 					</tbody>
 				</table>
 			);
@@ -31,27 +36,30 @@ const Box = React.createClass({
 });
 
 const Row = React.createClass({
+	rowCellOnClick: function (x, y) {
+		this.props.changeStateOfCell(x, y);
+	},
 	render: function () {
 		const cells = this.props.cells;
 		const rowIndex = this.props.rowindex;
 		return (<tr>
 				{cells.map(function(cell, index){
-					return <Cell item={cell} rowindex={rowIndex} cellindex={index} />;
-				})}
+					return <Cell item={cell} rowCellOnClick={this.rowCellOnClick} rowindex={rowIndex} cellindex={index} />;
+				}, this)}
 			</tr>);
 	}
 });
 
 const Cell = React.createClass({
-	hendlerOnClick: function (x, y) {
-		console.log(x, y);
+	handlerOnClick: function () {
+		const x = this.props.rowindex;
+		const y = this.props.cellindex;
+		this.props.rowCellOnClick(x, y);
 	},
 	render: function () {
 		const item = this.props.item;
-		const rowIndex = this.props.rowindex;
-		const cellIndex = this.props.cellindex;
 
-		return (<td onClick={this.hendlerOnClick(rowIndex, cellIndex)}>| {item} |</td>);
+		return (<td onClick={this.handlerOnClick}>| {item} |</td>);
 	}
 });
 
