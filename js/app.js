4b44c490ee014eaee1905207a925d2acc067ccd6
Rostyslav <rostowner@gmail.com>
2016-03-17 18:09:47 +0200

    add CHANGE PLAYER button

--- js/app.js
+++ js/app.js
@@ -7,12 +7,10 @@ const cellStates = {
 };
 
 const Box = React.createClass({
-	handlerOnClick: function (x, y) {
-		console.log(x, y);
-	},
 	getInitialState: function () {
 		return {
 			size: 3,
+			player: cellStates.x,
 			matrix: [
 				[cellStates._, cellStates._, cellStates._], 
 				[cellStates._, cellStates._, cellStates._], 
@@ -20,20 +18,28 @@ const Box = React.createClass({
 			]
 		};
 	},
+	changePlayer: function () {
+		const player = (this.state.player === cellStates.x)? cellStates.o : cellStates.x;
+		this.setState({player: player});
+	},
 	changeStateOfCell: function (x, y) {
+		const playerSimbol = (this.state.player == cellStates.x) ? cellStates.x : cellStates.o;
 		var matrix = this.state.matrix;
-		matrix[x][y] = cellStates.x;
+		matrix[x][y] = playerSimbol;
 		this.setState({matrix: matrix});
 	},
 	render: function () {
 		const matrix = this.state.matrix;
-		return (<table>
+		return (<div>
+				<table>
 					<tbody>
 						{matrix.map(function(row, index){ 
 							return <Row cells={row} changeStateOfCell={this.changeStateOfCell} rowindex={index} />; 
 						}, this)}
 					</tbody>
 				</table>
+				<Player changePlayer={this.changePlayer} player={this.state.player} />
+			</div>
 			);
 	}
 });
@@ -65,7 +71,20 @@ const Cell = React.createClass({
 	}
 });
 
+const Player = React.createClass({
+	changePlayer: function () {
+		this.props.changePlayer();
+	},
+	render: function () {
+		const player = this.props.player;
+		return (<div>
+			<h1>Player: {player}</h1>
+			<button onClick={this.changePlayer}>Change Player</ button>
+			</div>);
+	}
+});
+
 ReactDOM.render(
-  <Box />,
-  document.getElementById('xo')
+	<Box />,
+  	document.getElementById('xo')
 );
